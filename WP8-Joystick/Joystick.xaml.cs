ed38e40aa508894fdc9643292dd9902dbb577e31
Sascha Corti <sascha@corti.com>
2012-12-03 09:53:21 +0100

    Added MS-Pl

--- WP8-Joystick/Joystick.xaml.cs
+++ WP8-Joystick/Joystick.xaml.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿//
+// Written in 2012 by Sascha Corti.
+//
+// Licensed under the Microsoft Public License (Ms-PL).
+// You may se this file in compliance with the License.
+// Obtain a copy of the License at:
+//
+//    http://opensource.org/licenses/Ms-PL.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
